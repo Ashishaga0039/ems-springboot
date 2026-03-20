ac61b18185b1ae58f560a003fa21b99fc60e229d
Ashish Agarwal <Ashish039aga@gmail.com>
2026-03-21 01:06:25 +0530

    Add files via upload

---
+++ src/main/java/com/ems/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ems.repository;
+
+import com.ems.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
